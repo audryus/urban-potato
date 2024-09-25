86937c6b1a282aabe96085ae0f4904d9a8edbd71
audryus <audryus@gmail.com>
2024-09-25 17:55:40 -0300

    feat initial commit
    Criação do projeto no start.spring.io

---
+++ src/main/java/desafio/urban_potato/UrbanPotatoApplication.java
@@ -0,0 +1,13 @@
+package desafio.urban_potato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanPotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanPotatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafio/urban_potato/UrbanPotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.urban_potato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanPotatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
